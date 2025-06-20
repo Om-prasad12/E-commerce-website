5a4431a38c23c36939a713b2119de456c0e95bf0
Om Prasad <omprasadsahu9090@gmail.com>
2025-06-20 23:06:18 +0530

    feat(server): setup initial backend with Express

---
+++ server/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port=process.env.PORT || 8000;
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+
+
+app.listen(port,(req, res)=> { 
+    console.log("Connection has been made")
+})
+
+
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(cookieParser());
+
+const userRouter=require('./Routers/userRouter');
+const prodRouter=require('./Routers/prodRouter');
+const authRouter=require('./Routers/authRouter');
+
+app.use('/prod',prodRouter);
+app.use('/user',userRouter);
+app.use('/auth',authRouter); 
+
+
+
+
+app.use((req,res)=>{
+    res.status(404).send('404 Page not found')
+})
