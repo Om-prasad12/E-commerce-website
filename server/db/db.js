c73cc31bfd9bc5396a9f6af58d76529048565db5
Om Prasad <omprasadsahu12@gmail.com>
2025-08-14 13:56:13 +0530

    chore(server): increase API listening time

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose')
 
 
 const _db=process.env.DB
-mongoose.connect(_db, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(_db, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+  bufferMaxEntries: 0,
+  serverSelectionTimeoutMS: 30000,
+  socketTimeoutMS: 45000,
+  bufferCommands: false
+})
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
